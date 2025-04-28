adc6fc01fd6c731356dfecb9094563b9ae52c791
neoo-n <dana.vauthey@gmail.com>
2025-04-28 15:10:10 +0200

    ex01 done

---
+++ ex01/Fixed.hpp
@@ -0,0 +1,42 @@
+/******************************************************************************/
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dvauthey <dvauthey@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/26 14:20:18 by dvauthey          #+#    #+#             */
+/*   Updated: 2025/04/26 14:30:07 by dvauthey         ###   ########.fr       */
+/*                                                                            */
+/******************************************************************************/
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+# include <cmath>
+
+class Fixed
+{
+	private:
+		int					val;
+		static const int	nb_bits;
+
+	public:
+		Fixed(void);
+		Fixed(const Fixed &obj);
+		Fixed(const int i);
+		Fixed(const float i);
+		Fixed &operator=(const Fixed &obj);
+		~Fixed(void);
+
+		int		getRawBits(void) const;
+		void	setRawBits(int const raw);
+		float	toFloat(void) const;
+		int		toInt(void) const;
+
+};
+
+std::ostream	&operator<<(std::ostream &os, const Fixed &nb);
+
+#endif
---
+++ ex01/main.cpp
@@ -0,0 +1,35 @@
+/******************************************************************************/
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dvauthey <dvauthey@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/26 14:37:00 by dvauthey          #+#    #+#             */
+/*   Updated: 2025/04/26 14:37:56 by dvauthey         ###   ########.fr       */
+/*                                                                            */
+/******************************************************************************/
+
+#include "Fixed.hpp"
+
+int	main(void)
+{
+	Fixed a;
+	Fixed const b(10);
+	Fixed const c(42.42f);
+	Fixed const d(b);
+
+	a = Fixed(1234.4321f);
+
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << b << std::endl;
+	std::cout << "c is " << c << std::endl;
+	std::cout << "d is " << d << std::endl;
+
+	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+	return (0);
+}
+ 
